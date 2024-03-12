c686f5c1275a51aa8110e8e601aeb2dff3b1a8f5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-03-12 22:36:46 +0100

    Added two similar functions that instead replace all spaces with + in pushGiphyGif.js  Wrote the code that fetch all the necessary gifs base on weather conditions

--- src/pushGiphyGif.js
+++ src/pushGiphyGif.js
@@ -1,4 +1,5 @@
 import { cityInfoMinified, searchedCity } from "./getWeatherInfo";
+import { giphyApi } from "./loginApi";
 
 function changeGif() {
     if (searchedCity !== "") {
@@ -6,8 +7,56 @@ function changeGif() {
     }
 }
 
-function gifLoop() {
+function removeAllSpaces(number) {
+    let strTemporary = cityInfoMinified["next" + number][1];
+    strTemporary = strTemporary.replace(" ", "+");
 
+    return strTemporary;
+}
+
+function removeAllSpaces2() {
+    let strTemporary = cityInfoMinified["current"][1];
+    strTemporary = strTemporary.replace(" ", "+");
+
+    return strTemporary;
+}
+
+async function gifLoop() {
+    let allGif = document.querySelectorAll(".gif");
+
+    for (let i = 0; i < 3; i++) {
+        let response = await fetch(
+            "https://api.giphy.com/v1/gifs/translate?api_key=" +
+                giphyApi +
+                "&s=" +
+                removeAllSpaces(i),
+            {
+                mode: "cors",
+            },
+        );
+
+        response = await response.json();
+
+
+        console.log(response);
+        allGif[i].src = response.data.images.original.url;
+        if (i === 0) {
+            response = await fetch(
+                "https://api.giphy.com/v1/gifs/translate?api_key=" +
+                    giphyApi +
+                    "&s=" +
+                    removeAllSpaces2(),
+                {
+                    mode: "cors",
+                },
+            );
+
+            response = await response.json();
+
+            console.log(response);
+            allGif[3].src = response.data.images.original.url;
+        }
+    }
 }
 
 export { changeGif };

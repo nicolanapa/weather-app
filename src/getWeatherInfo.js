a6f5cdb3e96d610ef20b2fbd948cb157cd0a4ffb
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-03-12 20:27:39 +0100

    Coded processCityInfo() function which creates a new object with the only necessary values

--- src/getWeatherInfo.js
+++ src/getWeatherInfo.js
@@ -2,6 +2,29 @@ import { weatherApi } from "./loginApi";
 
 let searchedCity = "";
 let cityInfo = "";
+let cityInfoMinified = "";
+
+function dateChecker() {
+    // Creates an variable with today's date
+    let todayDate = new Date();
+    if (todayDate.getMonth() <= 9 && todayDate.getDate() <= 9) {
+        todayDate = todayDate.getFullYear() + "-0" + (todayDate.getMonth() + 1) + "-0" + todayDate.getDate();
+        console.log("Oggi è il " + todayDate);
+    } else if (todayDate.getMonth() <= 9 && todayDate.getDate() >= 9) {
+        todayDate = todayDate.getFullYear() + "-0" + (todayDate.getMonth() + 1) + "-" + todayDate.getDate();
+        console.log("Oggi è il " + todayDate);
+    } else if (todayDate.getDate() <= 9 && todayDate.getMonth() >= 9) {
+        todayDate = todayDate.getFullYear() + "-" + (todayDate.getMonth() + 1) + "-0" + todayDate.getDate();
+        console.log("Oggi è il " + todayDate);
+    } else {
+        todayDate = todayDate.getFullYear() + "-" + (todayDate.getMonth() + 1) + "-" + todayDate.getDate();
+        console.log("Oggi è il " + todayDate);
+    }
+    todayDate = String(todayDate);
+    return todayDate;
+}
+
+console.log(dateChecker());
 
 function searchCity() {
     let form = document.querySelector(".form-search");
@@ -29,7 +52,34 @@ async function getCityInfo() {
 }
 
 function processCityInfo() {
-    
+    //new object that only takes the needed values
+    cityInfoMinified = {
+        // [city name, city country]
+        location: [cityInfo.location.name, cityInfo.location.country],
+        // [current day, current weather conditin, current icon file, current temperature]
+        current: [dateChecker(), cityInfo.current.condition.text, cityInfo.current.condition.icon, cityInfo.temp_c],
+        next0: [
+            // [date, weather condition, icon file, average temperature]
+            cityInfo["forecast"]["forecastday"]["0"]["date"],
+            cityInfo["forecast"]["forecastday"]["0"]["day"]["condition"]["text"],
+            cityInfo["forecast"]["forecastday"]["0"]["day"]["condition"]["icon"],
+            cityInfo["forecast"]["forecastday"]["0"]["day"]["avgtemp_c"],
+        ],
+        next1: [
+            cityInfo["forecast"]["forecastday"]["1"]["date"],
+            cityInfo["forecast"]["forecastday"]["1"]["day"]["condition"]["text"],
+            cityInfo["forecast"]["forecastday"]["1"]["day"]["condition"]["icon"],
+            cityInfo["forecast"]["forecastday"]["1"]["day"]["avgtemp_c"],
+        ],
+        next2: [
+            cityInfo["forecast"]["forecastday"]["2"]["date"],
+            cityInfo["forecast"]["forecastday"]["2"]["day"]["condition"]["text"],
+            cityInfo["forecast"]["forecastday"]["2"]["day"]["condition"]["icon"],
+            cityInfo["forecast"]["forecastday"]["2"]["day"]["avgtemp_c"],
+        ],
+    };
+
+    console.log(cityInfoMinified);
 }
 
 export { cityInfo };
